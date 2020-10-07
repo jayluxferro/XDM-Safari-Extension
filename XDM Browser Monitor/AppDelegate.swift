3f0953be47ccf58f376222ae520559c631aaf504
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-10-07 01:10:48 +0000

    initial commit

---
+++ XDM Browser Monitor/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  XDM Browser Monitor
+//
+//  Created by Jay on 07/10/2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
